3be6a9e1523c35f252eb5957d8602545426d68d3
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-16 03:48:39 -0400

    stack.yaml, cabal, formatting/simple changes to WebRoutes.hs

--- src/React/Flux/Router/WebRoutes.hs
+++ src/React/Flux/Router/WebRoutes.hs
@@ -1,38 +1,51 @@
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 #ifdef __GHCJS__
 {-# LANGUAGE ForeignFunctionInterface, JavaScriptFFI #-}
 #endif
--- |
 
-module React.Flux.Router.WebRoutes
-              ( setLocationHash
-              , getLocationHash
-              , onLocationHashChange
-              , actionRoute
-              , childRoutePath
-              , initRouter
-              , storeRouter
-              ) where
+-- | Ripped almost directly from react-flux examples/routing.
+-- Not sure why this isn't in an existing repository by itself.
+-- Re-formatted to my liking.
+
+module React.Flux.Router.WebRoutes (
+  setLocationHash,
+  getLocationHash,
+  onLocationHashChange,
+  actionRoute,
+  childRoutePath,
+  initRouter,
+  storeRouter
+) where
+
+
 
 import           React.Flux
 
-import qualified Data.Text as T
-import qualified Data.ByteString.Char8 as BC
 import           Control.Monad.IO.Class (liftIO)
-import qualified Web.Routes as WR
+import qualified Data.ByteString.Char8  as BC (pack)
+import           Data.Text              (Text)
+import qualified Data.Text              as T (cons, isPrefixOf)
+import qualified Web.Routes             as WebRoutes (PathInfo, decodePathInfo,
+                                                      mkSitePI, runRouteT,
+                                                      runSite, toPathInfo,
+                                                      toPathSegments)
+
 
 
 #ifdef __GHCJS__
 
-import           Control.Monad (liftM)
-import qualified Data.JSString as JSS
+import           Control.Monad          (liftM)
+import qualified Data.JSString          as JSS
 import           GHCJS.Foreign.Callback
-import           GHCJS.Types (JSString, JSVal)
+import           GHCJS.Types            (JSString, JSVal)
 import           Unsafe.Coerce
 
 #endif
+
+
 
 #ifdef __GHCJS__
 
@@ -54,7 +67,7 @@ setLocationHash = js_setLocationHash . JSS.pack
 getLocationHash :: IO (Maybe String)
 getLocationHash = do
   rt <- liftM JSS.unpack js_getLocationHash
-  return $ case rt of
+  pure $ case rt of
     "" -> Nothing
     _ -> Just rt
 
@@ -66,41 +79,49 @@ onLocationHashChange fn = do
 # else
 
 setLocationHash :: String -> IO ()
-setLocationHash _ = return ()
+setLocationHash = const $ pure ()
 
 getLocationHash :: IO (Maybe String)
-getLocationHash = return Nothing
+getLocationHash = pure Nothing
 
 onLocationHashChange :: (String -> IO ()) -> IO ()
-onLocationHashChange _ = return ()
+onLocationHashChange = const $ pure ()
 
 #endif
 
-childRoutePath :: WR.PathInfo action => action -> [T.Text]
-childRoutePath = WR.toPathSegments
 
-actionRoute :: WR.PathInfo action => Maybe ([T.Text] -> T.Text) -> action -> T.Text
+
+childRoutePath :: WebRoutes.PathInfo action => action -> [Text]
+childRoutePath = WebRoutes.toPathSegments
+
+
+
+actionRoute :: WebRoutes.PathInfo action => Maybe ([Text] -> Text) -> action -> Text
 actionRoute mparentRouter action =
   frag
   where
-    path = maybe (WR.toPathInfo action) ($ childRoutePath action) mparentRouter
+    path = maybe (WebRoutes.toPathInfo action) ($ childRoutePath action) mparentRouter
     frag = if "#" `T.isPrefixOf` path
            then path
            else T.cons '#' path
 
-initRouter :: ([T.Text] -> IO ()) -> IO ()
+
+
+initRouter :: ([Text] -> IO ()) -> IO ()
 initRouter router =
-  onLocationHashChange $ router . stripHash . WR.decodePathInfo . BC.pack
+  onLocationHashChange $ router . stripHash . WebRoutes.decodePathInfo . BC.pack
   where
     stripHash ("#":path) = path
     stripHash path = path
 
-storeRouter :: (StoreData store, WR.PathInfo (StoreAction store)) =>
-               ReactStore store -> ([T.Text] -> IO ())
+
+
+storeRouter :: (StoreData store, WebRoutes.PathInfo (StoreAction store)) =>
+               ReactStore store -> ([Text] -> IO ())
 storeRouter store =
-  let site = WR.mkSitePI $ WR.runRouteT $ routerAlterStore
+  let site = WebRoutes.mkSitePI $ WebRoutes.runRouteT $ routerAlterStore
   in
-    \rt -> either (const $ return ()) id $ WR.runSite "" site rt
+    \rt -> either (const $ pure ()) id $ WebRoutes.runSite "" site rt
   where
     routerAlterStore action =
       liftIO $ alterStore store action
